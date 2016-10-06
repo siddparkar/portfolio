4ccc5f4345a0e2ec766155604f3378a5117dce51
Siddhant Parkar <Siddhant Parkar>
2016-10-05 21:17:15 -0400

    First commit

---
+++ test.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+ $("#main-image").hide(1000).show(4000);
+ $("#myname").hide(800).show(3200);
+ $("#awesome").hide(600).show(2400);
+ $("#notice").hide(400).show(1600);
+ $("#JSButton").hide(200).show(800);
+
+});
